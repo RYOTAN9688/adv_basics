a6f870661532bb7bab888835097b9a5e83f03e70
NAKAO RYOTA <premium.developer1122.gmail.com>
2023-09-25 07:59:58 +0900

    results summary data addted

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -1,4 +1,5 @@
 import 'package:adv_basics/data/questions.dart';
+import 'package:adv_basics/results_screen.dart';
 import 'package:flutter/material.dart';
 
 import 'package:adv_basics/questions_screen.dart';
@@ -28,7 +29,7 @@ class _QuizState extends State<Quiz> {
     if (selectedAnswer.length == questions.length) {
       setState(() {
         selectedAnswer = [];
-        activeScreen = 'start_screen';
+        activeScreen = 'results-screen';
       });
     }
   }
@@ -39,6 +40,11 @@ class _QuizState extends State<Quiz> {
 
     if (activeScreen == 'questions_screen') {
       screenWidget = QuestionsScreen(onSelectedAnswer: chooseAnswer);
+    }
+    if (activeScreen == 'results-screen') {
+      screenWidget = ResultsScreen(
+        chosenAnswers: selectedAnswer,
+      );
     }
 
     return Container(
